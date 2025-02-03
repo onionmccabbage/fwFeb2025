ce6dd5471dfdda90408600b961836b48d1803315
onionmccabbage <onionmccabbage@live.com>
2025-02-03 12:31:42 +0000

    sample data analysis (simple)

---
+++ util.py
@@ -0,0 +1,14 @@
+def addThem(a,b):
+    return a+b
+
+def squareIt(n):
+    return n**2 # here the double asterisk means raise to the power...
+
+
+
+if __name__ == '__main__':
+    # we use the above line to protect code
+    #e.g. we ONLY want the following to run when THIS module is called directly
+    # i.e. we exercise this module here, condifent the exercise code will not be used on import
+    print(f'This module is called {__name__}')
+    addThem(3,2)
---
+++ weather.py
@@ -0,0 +1,29 @@
+import util
+import json # this is part of the Python standard library
+
+
+# we need to load in some external data
+def grabJSON():
+    '''Load some JSON data for us to work with
+    Here we load weather dat for Athens'''
+    with open('athens_w.json', 'r') as fin:
+        ath = fin.read() # read inall the content
+    # function do not need a 'return' statement
+    # if we choose to return something, that is the end of the function
+    return ath
+
+# then we need to grab the bits we're intersted in
+def handleData():
+    '''Take the loaded data and start analysing it'''
+    result = grabJSON()
+    # we can convert this into a Pyuthon structure
+    results_obj = json.loads(result)
+    # challenge: print the 'coord' value of our dict
+    print(f'{results_obj['coord']}')
+    # print(type(results_obj), results_obj)
+
+# make them into a new pretty structure
+
+if __name__ == '__main__':
+    print( grabJSON() )
+    handleData()
