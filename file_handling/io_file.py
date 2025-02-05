676184e3b32a6f6da63383a7affe7b2768c1f8df
onionmccabbage <onionmccabbage@live.com>
2025-02-05 13:03:01 +0000

    lunch day 3

---
+++ file_handling/io_file.py
@@ -0,0 +1,11 @@
+# read a whole file e,g, 40,000 rows by 30 columns approx
+# each row may reerence a table with ~100,000 rows
+
+# run pipeline, adds to new DB, or to country ( USA, Canada )
+# probably aim to keep keyword csv together
+
+# 1. 40,000 is too many for the website
+#    so only go to row x
+# 2. read a row from a table, then look up in another table (lots)
+#    given a keyword, which page do we expect to rank for this keyword
+
